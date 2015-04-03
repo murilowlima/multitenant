a9f7986aad3e63595083d6dc5085d31457a2321e
Murilo Lima <murilo.wlima@gmail.com>
2015-04-03 17:15:32 -0300

    Added missing file

---
+++ src/Controller/Component/TenancyComponent.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace MultiTenant\Controller\Component;
+
+use Cake\Controller\Component;
+use Cake\Event\Event;
+use Cake\Core\Exception\Exception;
+use MultiTenant\Core\MTApp;
+
+/**
+ * CakePHP TenancyComponent
+ * @author Murilo Lima https://github.com/murilowlima/
+ */
+class TenancyComponent extends Component {
+
+    public $components = array();
+    protected $_defaultConfig = [
+        'qualifierKey' => null,
+        'strategy' => null,
+        'redirect' => null
+    ];
+
+    public function beforeFilter(Event $event) {
+        MTApp::$handler = $this;
+        $context = $this->getContext();
+        if ($context == 'tenant') {
+            $tenant = $this->getTenant();
+            if ($tenant == null) {
+                return $this->redirect();
+            }
+        }
+    }
+
+    public function getTenant() {
+        return MTApp::tenant();
+    }
+
+    public function setTenant($tenant) {
+        MTApp::setTenant($tenant);
+    }
+
+    public function redirect() {
+        $controller = $this->_registry->getController();
+        return $controller->redirect($this->config('redirect'));
+    }
+
+    public function isTenancyContext() {
+        $controller = $this->_registry->getController();
+        $className = get_class($controller);
+
+        if (!method_exists($className, 'isGlobalContext')) {
+            throw new Exception('Controller must implement an isGlobalContext() method.');
+        }
+
+        return !$controller->isGlobalContext();
+    }
+
+    public function isPrimary() {
+        return !$this->isTenancyContext();
+    }
+
+    public function getContext() {
+        $controller = $this->_registry->getController();
+        if (!$this->isTenancyContext($controller)) {
+            return 'global';
+        }
+
+        return 'tenant';
+    }
+
+    public function unsetTenant() {
+        MTApp::unsetTenant();
+    }
+
+}
